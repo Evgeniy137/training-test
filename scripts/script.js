4d131beda4cd9c45d999e81360b4e2bfc1f26878
Evgeniy <vavilon11111111@gmail.com>
2020-07-18 18:19:06 +0300

    Work with cycles

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 "use srict";
+/* test1 */
 
-
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+/* let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
 let personalMovieOB = {
     count: numberOfFilms,
@@ -19,7 +19,38 @@ let lastFilmsStep2 = prompt("Один из последних просмотре
 
 personalMovieOB.movies[lastFilmsStep1] = filMsassessmentStep1;
 
+console.log(personalMovieOB); */
+
+/* test2 */
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+if(numberOfFilms < 10) {
+    prompt("Просмотрено довольно мало фильмов");
+    } else if (numberOfFilms >= 10 || numberOfFilms <= 30) {
+        prompt("Вы классический зритель");
+        } else if (numberOfFilms > 30) {
+            prompt("Вы киноман");
+            } else {
+                prompt("Произошла ошибка");
+                }
+
+let personalMovieOB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for(let i = 0; i < 2; i++) {
+let lastFilmsStep1 = prompt("Один из последних просмотренных фильмов", ""),
+    filMsassessmentStep1 = prompt("На сколько отцените его", "");
+
+if(lastFilmsStep1 != null && filMsassessmentStep1 != null && lastFilmsStep1 != "" && filMsassessmentStep1 != "" && lastFilmsStep1.length < 50) {
+    personalMovieOB.movies[lastFilmsStep1] = filMsassessmentStep1; 
+    console.log("done");
+    }else {
+    i--;
+    }    
+} 
 
 
-
-console.log(personalMovieOB);
